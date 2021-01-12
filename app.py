7bfdbd7a4b47092589bf3c607257fa2fd0b3dfa6
evelyn <evelynrhee103@gmail.com>
2021-01-11 22:15:20 -0800

    madlib

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-num = int(input('Enter num'))
-print('hello', num + 5)
+print (7/4)
+print (2** 3)
+a = 'abcdefg'
+print(a)
+print(a[2:])
+print(a[:3])
+print(a[::2])
---
+++ madlib.py
@@ -0,0 +1,9 @@
+adj1 = input("first adjective: ")
+noun1 = input("first noun: ")
+noun2 = input("second noun: ")
+verb1 = input("first verb: ")
+noun3 = input("third noun: ")
+
+madlib = f"Today was a {adj1} day. There were lots of {noun1} and {noun2} at the zoo. \ I love to {verb1} and swim along the {noun3} "
+
+print(madlib)
